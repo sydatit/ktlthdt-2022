32bc50a79040daea75be2a840caa20246761d067
Son Nguyen <sonnguyen@mission.plus>
2022-09-26 12:40:53 +0700

    So Fibo thu N

---
+++ LuyenTap/src/dequy/T208_SoFiboThuN.java
@@ -0,0 +1,25 @@
+package dequy;
+
+import java.util.Scanner;
+
+public class T208_SoFiboThuN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int repeat = scanner.nextInt();
+        while (repeat --> 0){
+            int n = scanner.nextInt();
+
+
+        }
+    }
+
+    private int[][] multiMatrix(int[][] matrixA, int[][] matrixB){
+        int xSize = matrixA.length;
+        int ySize = matrixA[0].length;
+        int[][] matrixRes = new int[xSize][xSize];
+        for (int i = 0; i < xSize; i++) {
+
+        }
+        return matrixRes;
+    }
+}
